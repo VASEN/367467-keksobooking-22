5e2ac90babca20566aa07ec1791e0d7e171f6e27
VALERY ZAICHKO <zaichkovv@mail.ru>
2021-01-31 13:44:06 +0300

    Вносит правки в функции поска случайного числа после доработки

--- js/main.js
+++ js/main.js
@@ -1,29 +1,33 @@
+'use strict';
+
 const RANGE_START = 0;
-const RANGE_END = 10;
+const RANGE_END = 1;
 const FLOAT_LENGTH = 6;
 
-const numberToInteger = function (number) {
-  return parseInt(number);
-}
-
 const getRandomRangeInt = function (start, end) {
-  start = numberToInteger(start);
-  end = numberToInteger(end);
-  if (start >= 0 && end >= 0 && start !== end) {
-    return (start < end)
-      ? parseInt(Math.random() * (end - start) + start)
-      : parseInt(Math.random() * (start - end) + end);
+  start = Math.ceil(start);
+  end = Math.floor(end);
+  if (start > end) {
+    [start, end] = [end, start];
   }
-  return 'Неверно задан диапазон!';
+  if (start >= 0 && end >= 0 && start !== end) {
+    if ((end - start) !== 1) {
+      return Math.floor(Math.random() * (end - start) + start);
+    } else {
+      throw new Error('Слишком маленький диапазон!');
+    }
+  }
+  throw new Error('Неверно задан диапазон!');
 }
 
 const getRandomRangeFloat = function (start, end, length = 2) {
-  if (start >= 0 && end >= 0 && start !== end) {
-    return (start < end)
-      ? +(Math.random() * (end - start) + start).toFixed(length)
-      : +(Math.random() * (start - end) + end).toFixed(length);
+  if (start > end) {
+    [start, end] = [end, start];
   }
-  return 'Неверно задан диапазон!';
+  if (start >= 0 && end >= 0 && start !== end) {
+    return (Math.random() * (end - start) + start).toFixed(length);
+  }
+  throw new Error('Неверно задан диапазон!');
 }
 
 getRandomRangeInt(RANGE_START, RANGE_END);
